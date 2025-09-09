731dcd808b9ab9045215c2f1c7f0376d27b91387
Camus-Jugieau <hippolytecamusjugieau@gmail.com>
2025-09-09 19:19:29 +0200

    Ajout d'anti-aliasing

--- puissance4/src/main/java/game/board.java
+++ puissance4/src/main/java/game/board.java
@@ -10,6 +10,10 @@ class Plateau extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+
         int cellWidth = getWidth() / COLS;
         int cellHeight = getHeight() / ROWS;
         int cellSize = Math.min(cellWidth, cellHeight);
@@ -21,8 +25,8 @@ class Plateau extends JPanel {
         int xOffset = (getWidth() - boardWidth) / 2;      // centré
         int yOffset = getHeight() - boardHeight;
 
-        g.setColor(Color.BLUE);
-        g.fillRect(xOffset, yOffset, boardWidth, boardHeight); // fond bleu
+        g2d.setColor(Color.BLUE);
+        g2d.fillRect(xOffset, yOffset, boardWidth, boardHeight); // fond bleu
 
         // boucle sur toutes les cases
         for (int r = 0; r < ROWS; r++) {
@@ -31,8 +35,8 @@ class Plateau extends JPanel {
                 int y = yOffset + r * cellSize;
 
                 // case vide = trou blanc
-                g.setColor(Color.WHITE);
-                g.fillOval(x + 10, y + 10, cellSize - 20, cellSize - 20);
+                g2d.setColor(Color.WHITE);
+                g2d.fillOval(x + 10, y + 10, cellSize - 20, cellSize - 20);
             }
         }
     }
