b44dc413ca53171dbe873d0fed1e31bee633401c
dbgquivn <grenierloric@gmail.com>
2025-09-24 18:56:52 +0200

    réorganisation du projet

---
+++ puissance4/src/main/java/game/Main.java
@@ -0,0 +1,22 @@
+package game;
+
+public class main {
+    public static void main(String[] args) {
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        int screenwidth = gd.getDisplayMode().getWidth();
+        int screenheight = gd.getDisplayMode().getHeight();
+
+        double percent = 0.8;
+        int width = (int) (screenwidth * percent);
+        int height = (int) (screenheight * percent);
+
+        JFrame frame = new JFrame("Premiere fenetre");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(width,height);
+        frame.setLocationRelativeTo(null);
+
+        Plateau plateau = new Plateau();
+        frame.add(plateau).setLocation(screenwidth/2, 0);
+        frame.setVisible(true);
+    }
+}
