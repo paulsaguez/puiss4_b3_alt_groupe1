d99b9824c5f1a5a30867a54d309e2176156955c2
dbgquivn <grenierloric@gmail.com>
2025-10-07 18:42:06 +0200

    commit initial pour la création d'une game

---
+++ puissance4/src/main/java/game/Game.java
@@ -0,0 +1,13 @@
+package game;
+
+public class Game {
+
+    public int[][] plateau;
+    public Player player1;
+    public Player player2;
+
+    public Game() {
+        this.plateau = new int[6][7];
+    }
+
+}
