bfe2230be7658e9b8bd3094e4aa947271b69c9e5
dbgquivn <grenierloric@gmail.com>
2025-10-21 18:44:13 +0200

    fix bug sur le nombre de colonne

--- puissance4/src/main/java/game/Game.java
+++ puissance4/src/main/java/game/Game.java
@@ -10,25 +10,31 @@ public class Game {
     public Player activePlayer;
     public int[] lastOne;
 
-    public Game() {
+    public Game(Player player1, Player player2) {
         this.plateau = new int[6][7];
-        this.lastOne = new int[6];
-        for(int i = 0; i < 6; i++) {
+        this.lastOne = new int[7];
+        for(int i = 0; i < 7; i++) {
             this.lastOne[i] = -1;
         }
+        this.player1 = player1;
+        this.player2 = player2;
+        this.activePlayer = player1;
     }
 
-    public void play(int col){
+    public boolean play(int col){
         //1 représente un pion du joueur 1 et 2 un pion du joueur 2
-        if (col < 0 || col >= 7) return;
+        if (col < 0 || col >= 7) return false;
 
-        if (lastOne[col] >= 5) return;
+        if (lastOne[col] >= 5) return false;
 
         int nextRow = lastOne[col] + 1;
 
         plateau[nextRow][col] = (activePlayer.equals(player1)) ? 1 : 2;
 
         lastOne[col] = nextRow;
+        return true;
     }
+
+    
 
 }
