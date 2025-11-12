1efeae3323ee50b70b318edefbc5069e45b324b6
Camus-Jugieau <hippolytecamusjugieau@gmail.com>
2025-11-12 19:10:00 +0100

    Ajout fichier de test

---
+++ puissance4/src/test/java/game/GameTest.java
@@ -0,0 +1,24 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void play() {
+    }
+
+    @Test
+    void switchActivePlayer() {
+    }
+
+    @Test
+    void checkDraw() {
+    }
+
+    @Test
+    void checkVictory() {
+    }
+}
---
+++ puissance4/src/test/java/game/PlayerTest.java
@@ -0,0 +1,24 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getCouleur() {
+    }
+}
