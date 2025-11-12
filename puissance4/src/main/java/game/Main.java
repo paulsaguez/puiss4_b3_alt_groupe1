725a41305763c223677144300172b0f786304332
pauls <paul.saguez@etud.u-picardie.fr>
2025-11-12 18:31:18 +0100

    Mise en place du main

--- puissance4/src/main/java/game/Main.java
+++ puissance4/src/main/java/game/Main.java
@@ -1,25 +1,22 @@
 package game;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-        int screenwidth = gd.getDisplayMode().getWidth();
-        int screenheight = gd.getDisplayMode().getHeight();
+        SwingUtilities.invokeLater(() -> {
+            Player p1 = new Player("joueur1",PlayerColor.ROUGE, 1);
+            Player p2 = new Player("joueur1",PlayerColor.JAUNE, 2);
 
-        double percent = 0.8;
-        int width = (int) (screenwidth * percent);
-        int height = (int) (screenheight * percent);
+            Game game = new Game(p1, p2);
+            Plateau plateau = new Plateau(game);
 
-        JFrame frame = new JFrame("Premiere fenetre");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(width,height);
-        frame.setLocationRelativeTo(null);
-
-        Plateau plateau = new Plateau();
-        frame.add(plateau).setLocation(screenwidth/2, 0);
-        frame.setVisible(true);
+            JFrame frame = new JFrame("Puissance 4");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(700, 600);
+            frame.setResizable(false);
+            frame.add(plateau);
+            frame.setVisible(true);
+        });
     }
 }
